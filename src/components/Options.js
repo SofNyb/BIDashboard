6ea159bad8517e7b7d5524ff3e008f31c9040af7
SofNyb <sofi13a5@edu.zealand.dk>
2025-01-09 18:15:41 +0100

    Updated reportlink

--- src/components/Options.js
+++ src/components/Options.js
@@ -13,12 +13,7 @@ const Options = () => {
         "https://lookerstudio.google.com/embed/reporting/00db76df-9684-4433-992f-aeba8d33164e/page/jJ8YE",
     },
     {
-      label: "Les Deux",
-      value:
-        "https://lookerstudio.google.com/embed/reporting/09cc061d-5b2e-45c6-a9c0-bb7a0fca0c72/page/XJoUE",
-    },
-    {
-      label: "ÅBEN",
+      label: "Testrapport",
       value:
         "https://lookerstudio.google.com/embed/reporting/ab7a5abd-acd8-4db8-ad1c-8e2eab393422/page/WHoUE",
     },
